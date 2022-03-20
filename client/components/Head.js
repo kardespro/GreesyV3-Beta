d0b208449297505747bffc3f60110e2577ebde75
Nego <61694826+kardespro@users.noreply.github.com>
2022-03-20 18:48:16 +0400

    Create Head.js

---
+++ client/components/Head.js
@@ -0,0 +1,17 @@
+import negoHed from 'next/head'
+export default function Headd(){
+  return(
+    <>
+    <negoHed>
+
+
+
+    <title> Greesy | Best , Powerfull </title>
+
+
+    <link href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" rel="stylesheet" />
+
+    </negoHed>
+    </>
+  )
+}
