5a1abdd4f414ad89bf6c5ba923b7c0960bb7db03
Nego <61694826+kardespro@users.noreply.github.com>
2022-03-20 18:47:16 +0400

    Create Features.js

---
+++ client/components/Features.js
@@ -0,0 +1,39 @@
+
+export  default function Features(){
+  return(
+    <>
+     
+   
+<hr className="hr"/>
+    
+    
+    
+    <section class="text-center">
+  <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+    <ul class="grid grid-cols-2 gap-4 border-2 border-blue-600 rounded-xl lg:grid-cols-4">
+      <li class="p-8">
+        <p class="text-2xl font-extrabold text-teal-500">50</p>
+        <p class="text-white mt-1 text-lg font-medium">Guilds</p>
+      </li>
+
+      <li class="p-8">
+        <p class="text-2xl font-extrabold text-teal-500">190k+</p>
+        <p class="text-white mt-1 text-lg font-medium">Users</p>
+      </li>
+
+      <li class="p-8">
+        <p class="text-2xl font-extrabold text-teal-500">20+</p>
+        <p class="text-white mt-1 text-lg font-medium">Commands</p>
+      </li>
+
+      <li class="p-8">
+        <p class="text-2xl font-extrabold text-teal-500">1k+</p>
+        <p class="text-white mt-1 text-lg font-medium">Website Joins</p>
+      </li>
+    </ul>
+  </div>
+</section>
+ 
+    </>
+  )
+}
