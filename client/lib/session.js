aad12e41a3df0a9ab6ed742e10905eb252236d86
Nego <61694826+kardespro@users.noreply.github.com>
2022-03-20 18:46:37 +0400

    Create session.js

---
+++ client/lib/session.js
@@ -0,0 +1,3 @@
+import { withIronSession } from 'next-iron-session';
+export default function withSession(handler) {
+  return withIronSession(handler, { password: 'kPb18vU~y=iP>W2L1f%X2~+.a0AMXNkb5VxLfB}:Nn?8Ntk0T1', cookieName: 'greesy/session', maxAge: 2147483647, cookieOptions: { secure: process.env.NODE_ENV === "production" ? true : false } });};
